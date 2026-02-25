789323d5b5ad45f7b7b6f4b77f6cf8e0848c81ec
vanshikahardikshah <vanshikahardik.shah@sjsu.edu>
2026-02-24 16:04:23 -0800

    Create create_database.sql

---
+++ sql/create_database.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE shah;
+USE shah;
+
+CREATE TABLE Student (
+    SJSU_ID INT PRIMARY KEY,
+    Name VARCHAR(50),
+    Major VARCHAR(50)
+);
+
+INSERT INTO Student VALUES (017399395, 'Vanshika Shah', 'Computer Science');
+INSERT INTO Student VALUES (1002, 'Adarsh', 'Software Engineering');
+INSERT INTO Student VALUES (1003, 'Marcus', 'Data Science');
